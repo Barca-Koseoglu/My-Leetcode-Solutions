784a46e94fa42de96493d7a3f11f56ab21741f56
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-06-23 13:24:51 -0500

    Time: 425 ms, Memory: 12.6 MB - LeetHub

---
+++ move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        amount = nums.count(0)
+        for i in range(amount):
+            nums.remove(0)
+        nums.extend([0]*amount)
+        print(nums)
