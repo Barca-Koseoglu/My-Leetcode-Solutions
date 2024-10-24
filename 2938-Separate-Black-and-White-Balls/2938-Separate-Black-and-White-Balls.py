2761b86564d13c6a1cb010c4a5ac3a29a0b8a509
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-10-24 14:17:40 -0500

    Time: 64ms, Memory: 17.08 MB

--- 2938-Separate-Black-and-White-Balls/2938-Separate-Black-and-White-Balls.py
+++ 2938-Separate-Black-and-White-Balls/2938-Separate-Black-and-White-Balls.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumSteps(self, s: str) -> int:
         """
         This is a very simple problem. To solve it, I just thought about parsing through each element in the string (reversed) and if that
-        element is "1", aka the black ball, I would simply find out the number of steps it takes by subtracting the element's index by the 
+        element is "1", aka the black ball, I would simply find out the number of steps it takes to move it all the way right by subtracting the element's index by the 
         "count" variable. The "count" variable keeps track of the numbers of black balls in the right-most position. Any time a black ball
         is encountered, calculate the amount of steps required then add 1 to "count", because the count of the black balls in their correct position
         would increase by 1 after moving the black ball. Add all the values to the return variable, then return it.
