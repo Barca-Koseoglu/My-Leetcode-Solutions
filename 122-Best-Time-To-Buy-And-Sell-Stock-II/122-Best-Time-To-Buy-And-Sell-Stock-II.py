ffc2909054b594ba8044ddea1f82bc13c5db626e
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-10-29 18:05:28 -0500

    Update 122-Best-Time-To-Buy-And-Sell-Stock-II.py

--- 122-Best-Time-To-Buy-And-Sell-Stock-II/122-Best-Time-To-Buy-And-Sell-Stock-II.py
+++ 122-Best-Time-To-Buy-And-Sell-Stock-II/122-Best-Time-To-Buy-And-Sell-Stock-II.py
@@ -1,10 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
-        It took me a while to get this solution. At first, I thought of finding the maximum and minimum values and subtracting them, but 
-        later I realized that was the solution to the first question. After thinking about it, I figured out that if i buy and sell every time a profit 
-        was to be made, I could get the answer. It reminds me of a greedy algorithm. So what I did was enumerate thtough the list and whenever the number in 
-        front of the current number is bigger than it, I just subtracted them and added the value to my return variable, total.
+        What I did was enumerate through the list and whenever the number in front of the current number is bigger than it, I just subtracted them and added the value to my return variable.
         """
         total = 0 # return var
         for i in range(len(prices)-1): 
