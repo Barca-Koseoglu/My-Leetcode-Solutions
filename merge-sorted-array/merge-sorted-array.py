fb458093a6ace17de7f324b34aeffa761f32894d
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-11-21 10:47:52 -0600

    Update merge-sorted-array.py

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -1,13 +1,20 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(len(nums2)):
-            del nums1[-1]
-        nums1.extend(nums2)
-        nums1.sort()
+        # variables/pointers
+        m -= 1
+        n -= 1
+
+        for i in range(len(nums1)-1,-1,-1):
+
+            if n == -1:
+                break
+            
+            if m == -1 or nums1[m] < nums2[n]:
+                nums1[i] = nums2[n]
+                n -= 1
+            else:
+                nums1[i] = nums1[m]
+                m -= 1
