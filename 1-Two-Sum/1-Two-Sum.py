f86b3eb10a9ad62c82f793c79bed1a39f0fc74c1
Barca Koseoglu <barcakoseoglu@gmail.com>
2024-11-26 10:34:05 -0600

    Update 1-Two-Sum.py

--- 1-Two-Sum/1-Two-Sum.py
+++ 1-Two-Sum/1-Two-Sum.py
@@ -6,15 +6,7 @@ class Solution:
         store = {}
         
         for i, a in enumerate(nums):
-            if a not in store:
-                store[a] = i
-            elif 2*a == target:
-                return [i, store[a]]
-        
-        if target/2 in store:
-            del store[target/2]
-        
-        for i, a in enumerate(nums):
             if target - a in store:
                 return [i, store[target - a]]
-            
+            store[a] = i
+        
